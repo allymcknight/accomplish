35bc828d0b3719d4e080b6f8607bf1552e62aa1e
System Administrator <root@Allysons-iMac.local>
2017-01-22 22:16:43 -0800

    cleaned up comments

--- server.py
+++ server.py
@@ -8,11 +8,9 @@ import os
 
 app = Flask(__name__)
 
-# Required to use Flask sessions and the debug toolbar
+
 app.secret_key = os.environ['flask_app_key']
 
-# Normally, if you use an undefined variable in Jinja2, it fails silently.
-# This is horrible. Fix this so that, instead, it raises an error.
 app.jinja_env.undefined = StrictUndefined
 
 
@@ -24,13 +22,11 @@ def index():
 
 
 if __name__ == "__main__":
-    # We have to set debug=True here, since it has to be True at the point
-    # that we invoke the DebugToolbarExtension
+
     app.debug = True
 
     connect_to_db(app, 'postgresql:///accomplish')
 
-    # Use the DebugToolbar
     DebugToolbarExtension(app)
 
     app.run()
