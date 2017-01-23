20d65ab2fc9e5031ab938992e561e9d433902154
System Administrator <allyson.tong10@gmail.com>
2017-01-23 11:21:00 -0800

    added login funcs

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template, redirect, session
+from flask import Flask, render_template, redirect, session, request, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import connect_to_db, db
@@ -25,6 +25,29 @@ def login_form():
     """Shows register and login form"""
 
     return render_template('login.html')
+
+@app.route('/login_submit')
+def login_submit():
+
+    email = request.form.get('username')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        flash("We couldn't find your email, sorry. Try again.")
+        return redirect('/login')
+    if user.password != password:
+        flash("That's not the right password for this username. Try again.")
+        return redirect('/login')
+
+    session['user_id'] = user.user_id
+
+    flash("Welcome, %s" % user.name)
+    return redirect('/accomplishments/%s'% str(user.user_id))
+
+
+
 
 
 if __name__ == "__main__":
